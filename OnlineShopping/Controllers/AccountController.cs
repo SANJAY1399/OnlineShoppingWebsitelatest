c124517f13eb237ed96427a7c3b30abf1c1f4745
SANJAY1399 <sanjaysrinivas0172@gmail.com>
2021-11-16 11:45:25 +0530

    first

---
+++ OnlineShopping/Controllers/AccountController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using OnlineShopping.Models;
+namespace OnlineShopping.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        User u = new User();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(Login model)
+        {
+
+            
+            using (var context = new shoppingEntities())
+            {
+                //List<User> u = context.Users.Where(x => x.UserName == model.Username).ToList();
+                //int id = u.Find(x => x.UserName == model.Username).userID;
+                bool isvalid = context.Users.Any(x => x.UserName == model.Username && x.UserPassword == model.Password);
+                
+                if (isvalid)
+                {
+                    HttpCookie userIdCookie = new HttpCookie("uId");
+                    userIdCookie.Value = model.Username;
+                    userIdCookie.Expires = DateTime.Now.AddDays(10);
+                    Response.SetCookie(userIdCookie);
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+
+                    return RedirectToAction("Index","Home");
+                }
+                ModelState.AddModelError("", "Invalid username or password");
+                return View();
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User en)
+        {
+            using (shoppingEntities db = new shoppingEntities())
+            {
+                db.Users.Add(en);
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("login");
+        }
+
+        public ActionResult Reset()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Reset(Reset model)
+        {
+            using (var context = new shoppingEntities())
+            {
+                bool isvalid = context.Users.Any(x => x.UserName == model.Username);
+                if (isvalid)
+                {
+                    var cols = context.Users.Where(x => x.UserName.Equals(model.Username));
+                    foreach (User c in cols)
+                    {
+                        c.UserPassword = model.NewPassword;
+
+                    }
+                    context.SaveChanges();
+
+                    return RedirectToAction("login");
+                }
+                ModelState.AddModelError("", "Username does not exist!! ");
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login");
+        }
+
+    }
+}
+
---
+++ OnlineShopping/Models/Reset.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping.Models
+{
+    public class Reset
+    {
+        public int id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [RegularExpression("^[a-zA-Z']{1,50}", ErrorMessage = "Please enter valid Name")]
+        [DisplayName("Name")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Password mismatch")]
+        public string ConfirmPassword { get; set; }
+
+    }
+
+}
